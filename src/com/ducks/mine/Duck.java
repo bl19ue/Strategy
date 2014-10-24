2a140bd6884bdece6c860f67cf3d56deef88024c
Sumit Valecha <svalecha91@gmail.com>
2014-10-24 15:09:15 -0700

    Strategy 1.0.1

--- src/com/ducks/mine/Duck.java
+++ src/com/ducks/mine/Duck.java
@@ -1,11 +1,17 @@
 package com.ducks.mine;
 
-//This is just a demo of strategy pattern and it will not execute
+//But now we need ducks to fly() too
 
 public class Duck 
 {
 	void quack();
 	void swim();
 	void display();
+	
+	//So we add a fly() here
+	void fly();
+	//Now the problem is, all the ducks inherti this class, and all will be able to fly now
+	//which is a huge problem.
+	
 	//Other duck like methods
 }
