00a5fd0565f410d2a0176089d4db869fbf8881d3
Sumit Valecha <svalecha91@gmail.com>
2014-10-25 00:50:35 -0700

    Strategy 1.0.4

--- src/com/ducks/mine/Duck.java
+++ src/com/ducks/mine/Duck.java
@@ -7,19 +7,40 @@ package com.ducks.mine;
  * One for fly and one for quack
  * So we will create 2 classes, each set will contain all implementations of their respective behaviour
  * 
- *  Ex. one class that immplements quacking, another-squeaking, and silence too.
+ *  Ex. one class that implements quacking, another-squeaking, and silence too.
  *  
  *  ****************************************************************************************************
  *  So that teaches us "Program to an interface, not an implementation!!!"
  * */
 
+/*
+ * So the key is that, now the Duck will "delegate" its flying and quacking behaviour
+ * 
+ * So we should do that
+ * 
+ * 1. We'll add two instance variables for FlyBehaviour and QuackBehaviour
+ * */
+
 public class Duck 
 {
+	FlyBehaviour flyBehaviour;
+	QuackBehaviour quackBehaviour;
 	
 	void swim();
 	void display();
 	
-		
+	void performQuack()// These methods replace the fly() and quack()
+	{
+		quackBehaviour.quack();
+	}
+	void performFly()
+	{
+		flyBehaviour.fly();
+	}
+	//Now all we care about that our class is able to quack and fly
+	
+	//Go to the MallardDuck Class
+	
 	//Other duck like methods
 }
 
--- src/com/ducks/mine/MallardDuck.java
+++ src/com/ducks/mine/MallardDuck.java
@@ -1,13 +1,28 @@
 package com.ducks.mine;
 
-
+/*
+ * Now we have to take care about how the flyBehavior nstance variable are set
+ * 
+ * */
 
 public class MallardDuck extends Duck implements FlyBehaviour
 {
+	public MallardDuck()
+	{
+		quackBehaviour = new Quack();//<- Quack, class which implements QuackBehavior
+		flyBehaviour = new FlyWithWings();
+		
+		/*
+		 * Wait a second! what we are doing here!!
+		 * We are making a new instance of a concrete Quack and FlyWithThings!!
+		 * We should NOT program to an implementation!
+		 * */
+	}
 	void display()
 	{
 		//looks like a mallar duck
 	}
+	
 	@Override
 	public void fly()
 	{
